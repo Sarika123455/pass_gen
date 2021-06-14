f1071f9c8971baf771b321a311cc707651de49f4
Sarika123455 <priyashrivastava3010@gmail.com>
2021-06-14 15:09:23 +0530

    set

--- Society/settings.py
+++ Society/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'society1system.herokuapp.com']
+ALLOWED_HOSTS = ['society1system.herokuapp.com','localhost']
 
 
 # Application definition

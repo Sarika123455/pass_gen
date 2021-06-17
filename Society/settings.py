dbafbaf56b96bcf48a9b72d44c8ac8c07006b518
Sarika123455 <priyashrivastava3010@gmail.com>
2021-06-17 14:32:58 +0530

    file

--- Society/settings.py
+++ Society/settings.py
@@ -23,7 +23,7 @@ TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = '*o9t#aff83_$-y=9#+x*ff*ff%e(at$co%%5(fol7u-y1&&5vy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

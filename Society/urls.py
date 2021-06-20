7411e3aac9879fc88297f38e799bd338758fcd79
Sarika123455 <priyashrivastava3010@gmail.com>
2021-06-20 13:32:56 +0530

    url

--- Society/urls.py
+++ Society/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from admin_app import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -25,6 +26,8 @@ urlpatterns = [
     path("accounts/",include("django.contrib.auth.urls")),
     path("",include("admin_app.urls")),
     path("user/",include("user.urls")),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
+
    
 
 
